638a0988406766c4ca47f4e399948cc044cc9172
Alberto <arubalcaba@24hourfit.com>
2017-02-16 16:59:25 -0800

    Updated upload dare response activity

---
+++ app/src/main/java/com/dareu/mobile/net/dare/UploadDareResponseTask.java
@@ -0,0 +1,8 @@
+package com.dareu.mobile.net.dare;
+
+/**
+ * Created by jose.rubalcaba on 02/16/2017.
+ */
+
+public class UploadDareResponseTask {
+}
---
+++ app/src/main/java/com/dareu/mobile/net/request/UploadDareResponseRequest.java
@@ -0,0 +1,8 @@
+package com.dareu.mobile.net.request;
+
+/**
+ * Created by jose.rubalcaba on 02/16/2017.
+ */
+
+public class UploadDareResponseRequest {
+}
